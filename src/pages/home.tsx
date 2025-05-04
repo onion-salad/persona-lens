2f7bbe3ca68e23cec7f5c926c794d8fbf36343b2
onion-salad <kokimaru0502@yahoo.co.jp>
2025-05-04 22:45:15 +0900

    fix: 地球儀から静的なマーカーを削除

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -29,18 +29,7 @@ const WHITE_GLOBE_CONFIG: COBEOptions = {
   baseColor: [0.9, 0.9, 0.9], // ベースカラーを明るいグレーに
   markerColor: [0.3, 0.3, 0.3], // マーカーの色を濃いグレーに
   glowColor: [0.8, 0.8, 0.8], // グローの色を明るいグレーに
-  markers: [ // デフォルトのマーカーを使用
-    { location: [14.5995, 120.9842], size: 0.03 },
-    { location: [19.076, 72.8777], size: 0.1 },
-    { location: [23.8103, 90.4125], size: 0.05 },
-    { location: [30.0444, 31.2357], size: 0.07 },
-    { location: [39.9042, 116.4074], size: 0.08 },
-    { location: [-23.5505, -46.6333], size: 0.1 },
-    { location: [19.4326, -99.1332], size: 0.1 },
-    { location: [40.7128, -74.006], size: 0.1 },
-    { location: [34.6937, 135.5022], size: 0.05 },
-    { location: [41.0082, 28.9784], size: 0.06 },
-  ],
+  markers: [], // この配列を空にする
 };
 
 export function HomePage() {
